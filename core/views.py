c6249283c484ca96259d9c5d665c41b63b83cef8
Archit soni <architsoni@Archits-MacBook-Pro.local>
2025-11-11 20:58:18 +0530

    Added filters and Q object search feature in student_list view

--- core/views.py
+++ core/views.py
@@ -38,3 +38,32 @@ def student_delete(request, pk):
         student.delete()
         return redirect('student_list')
     return render(request, 'core/student_confirm_delete.html', {'student': student})
+
+#-------------******----- queries implementation------*******----------------
+
+from django.shortcuts import render             # Used to render HTML templates
+from django.db.models import Q                  # Used for complex queries (like OR conditions)
+from .models import Student                     # Import the Student model from current app
+def student_list(request):
+    # Start by fetching all student records from the database
+    students = Student.objects.all()
+    name = request.GET.get('name')                    # e.g. /students?name=Hemish
+    age_gt = request.GET.get('age_greater_than')      # e.g. /students?age_greater_than=18
+    age_lt = request.GET.get('age_less_than')         # e.g. /students?age_less_than=15
+    grade = request.GET.get('grade')                  # e.g. /students?grade=10
+    search = request.GET.get('search')                # e.g. /students?search=hem
+    if name:
+        students = students.filter(name__icontains=name)  # "icontains" → means "contains", ignoring case
+    if age_gt:
+        students = students.filter(age__gte=age_gt)   # Filter by age greater than or equal to a value
+    if age_lt:
+        students = students.filter(age__lte=age_lt)  # Filter by age less than or equal to a value
+    if grade:
+        students = students.filter(grade__iexact=grade) # Filter by grade (exact match but case-insensitive)
+
+   # USING Q OBJECT FOR COMPLEX FILTERS
+    if search:
+        students = students.filter(
+            Q(name__icontains=search) | Q(grade__icontains=search)
+        )
+    return render(request, 'core/student_list.html', {'students': students})
